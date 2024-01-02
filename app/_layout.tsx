53e74909738596cd8bd77a27ac84f4ca518ab0cd
roshan <roshanroy.rr@outlook.com>
2024-01-02 12:26:43 +0000

    setup screens

---
+++ app/_layout.tsx
@@ -0,0 +1,12 @@
+import { router } from "expo-router";
+import { useEffect } from "react";
+
+const InitialLayout = () => {
+  useEffect(() => {
+    router.replace
+  }, []);
+};
+
+export default () => {
+  return;
+};
