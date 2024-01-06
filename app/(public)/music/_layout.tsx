c4f6fecccee94f24b3224c4e1cc107460ee1ac4e
munaciella <franes88@gmail.com>
2024-01-06 00:51:47 +0000

    add grid to main feed

--- app/(public)/music/_layout.tsx
+++ app/(public)/music/_layout.tsx
@@ -3,8 +3,10 @@ import { Stack } from "expo-router";
 export default () => {
     return ( 
         <Stack>
-            <Stack.Screen name="index" />
-            <Stack.Screen name="[music_id]" />
+            <Stack.Screen name="index" options={
+                {headerShown: true, title: "Track"}}/>
+            <Stack.Screen name="[music_id]" options={
+                {headerShown: false, title: "Track"}}/>
         </Stack>
      );
 }
--- app/(public)/music/index.tsx
+++ app/(public)/music/index.tsx
@@ -1,49 +1,77 @@
-import { Image, ScrollView, Text, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { Link } from 'expo-router'
-import { useEffect, useState } from 'react'
-import getMusic from '../../../utils/api'
-import { Music } from '../../../types/front-end'
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Link } from "expo-router";
+import { useEffect, useState } from "react";
+import getMusic from "../../../utils/api";
+import { Music } from "../../../types/front-end";
 
 const Albums = () => {
-  const [music, setMusic] = useState<Music[]>([])
+  const [music, setMusic] = useState<Music[]>([]);
   useEffect(() => {
     const doThis = async () => {
-      const musicData = await getMusic()
-      setMusic(musicData)
-    }
-    doThis()
-  }, [])
+      const musicData = await getMusic();
+      setMusic(musicData);
+    };
+    doThis();
+  }, []);
 
+  // return (
   return (
     <SafeAreaView>
-      <ScrollView className="flex flex-col  bg-gray-300">
-      <View className='flex flex-wrap'>
-        {music.map((track: Music) => (
-          <View className="flex flex-col w-1/2 text-center items-center m-auto content-center bg-white p-10 rounded-lg">
-            <Link
-              href={{
-                pathname: `/(public)/music/${track.music_id}`,
-                params: { music_id: track.music_id },
-              }}
+      <ScrollView>
+        <View className="flex flex-row flex-wrap justify-between">
+          {music.map((track) => (
+            <View
+              className="w-1/2 p-4 bg-white rounded-lg items-center"
             >
-              <Image
-                className="w-40 h-40  justify-self-center self-center"
-                source={{ uri: track.album_img }}
-             
-              />
-            </Link>
-            <Text className="text-center py-5">{track.artist_names} </Text>
-            <Text className="text-center ">{track.name} </Text>
-          </View>
-        ))}
-          </View>
+              <Link
+                href={{
+                  pathname: `/(public)/music/${track.music_id}`,
+                  params: { music_id: track.music_id },
+                }}
+              >
+                <Image
+                  source={{ uri: track.album_img }}
+                  className="w-40 h-40  justify-self-center self-center rounded-lg"
+                />
+              </Link>
+              <Text className="text-center py-1">{track.artist_names}</Text>
+              <Text className="text-center">{track.name}</Text>
+            </View>
+          ))}
+        </View>
       </ScrollView>
-    
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Albums
+export default Albums;
+//     <SafeAreaView>
+//       <ScrollView className="flex flex-col  bg-gray-200">
+//       <View className='flex flex-wrap'>
+//         {music.map((track: Music) => (
+//           <View className="flex flex-col w-1/2 text-center items-center m-auto content-center bg-white p-10 rounded-lg">
+//             <Link
+//               href={{
+//                 pathname: `/(public)/music/${track.music_id}`,
+//                 params: { music_id: track.music_id },
+//               }}
+//             >
+//               <Image
+//                 className="w-40 h-40  justify-self-center self-center"
+//                 source={{ uri: track.album_img }}
 
+//               />
+//             </Link>
+//             <Text className="text-center py-5">{track.artist_names} </Text>
+//             <Text className="text-center ">{track.name} </Text>
+//           </View>
+//         ))}
+//           </View>
+//       </ScrollView>
 
+//     </SafeAreaView>
+//   )
+// }
+
+// export default Albums
