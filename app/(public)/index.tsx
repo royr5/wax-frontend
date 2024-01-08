698209f5207871e187d6d83ea74056c4232e1dff
ari-abendstern <ari.abendstern@gmx.com>
2024-01-08 11:37:00 +0000

    add logo image

--- app/(public)/index.tsx
+++ app/(public)/index.tsx
@@ -1,27 +1,29 @@
 import React from "react";
-import { Text } from "react-native";
+import { Image, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ClicketyBoo } from "../../components/reusable-components/ClicketyBoo";
 import { FormFieldText } from "../../components/reusable-components/FormFieldText";
 import { useState } from "react";
 import { router } from "expo-router";
 
-const [username, setUsername] = React.useState("");
-const [password, setPassword] = React.useState("");
+const [username, setUsername] = useState("");
+const [password, setPassword] = useState("");
 
 const Welcome = () => {
   return (
-    <SafeAreaView className="bg-white h-full">
-      <Text className="text-2xl">
-        Welcome to Wax
-      </Text>
-      <FormFieldText
-        label="Email"
-        setText={setUsername}
-        isRequired={true}
-        autoComplete="email"
-        enterKeyHint="next"
-      />
+    <SafeAreaView className="bg-[#15BA46] h-full">
+      <View className="w-full h-1/5 flex justify-center items-center px-10">
+      <Image source={require("../../assets/images/Wax-logo-transparent.png")} className="flex w-full h-full"/>
+      </View>
+      <View>
+        <FormFieldText
+          label="Email"
+          setText={setUsername}
+          isRequired={true}
+          autoComplete="email"
+          enterKeyHint="next"
+        />
+      </View>
       <FormFieldText
         label="Password"
         setText={setPassword}
@@ -30,7 +32,11 @@ const Welcome = () => {
         enterKeyHint="go"
       />
 
-      <ClicketyBoo onPressFunction={()=> router.replace('/music')} logo="⏯️" text="log in" />
+      <ClicketyBoo
+        onPressFunction={() => router.replace("/music")}
+        logo="⏯️"
+        text="log in"
+      />
     </SafeAreaView>
   );
 };
