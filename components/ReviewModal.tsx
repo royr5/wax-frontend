6943dd3e4431fe85e6dbf633b173b605a1db694e
roshan <roshanroy.rr@outlook.com>
2024-01-08 20:27:13 +0000

    add error handling

--- components/ReviewModal.tsx
+++ components/ReviewModal.tsx
@@ -5,6 +5,7 @@ import { postReview } from "../utils/api";
 import { useGlobalSearchParams } from "expo-router";
 import { useUserData } from "../app/contexts/UserContent";
 import { Review } from "../types/front-end";
+import { AntDesign } from "@expo/vector-icons";
 
 interface Iprops {
   setReviews: Function;
@@ -28,30 +29,42 @@ const ReviewModal = (props: Iprops) => {
   };
 
   const handleSubmit = async () => {
-    try {
-      const postedReview = await postReview(music_id as string, {
-        screen_name: user.username,
-        rating: rating,
-        review_title: title,
-        review_body: body,
-      });
+    if (title && body && rating) {
+      try {
+        const postedReview = await postReview(music_id as string, {
+          screen_name: user.username,
+          rating: rating,
+          review_title: title,
+          review_body: body,
+        });
 
-      setModalVisible(!modalVisible);
+        setModalVisible(!modalVisible);
+        setRating(0);
 
-      props.setReviews((currentReviews: Review[]) => {
-        return [
-          {
-            screen_name: user.username,
-            rating: rating,
-            review_title: title,
-            review_body: body,
-            created_at: new Date().toISOString(),
-          },
-          ...currentReviews,
-        ];
-      });
-    } catch (err) {
-      console.log("🚀 ~ file: ReviewModal.tsx:33 ~ handleSubmit ~ err:", err);
+        props.setReviews((currentReviews: Review[]) => {
+          return [
+            {
+              screen_name: user.username,
+              rating: rating,
+              review_title: title,
+              review_body: body,
+              created_at: new Date().toISOString(),
+            },
+            ...currentReviews,
+          ];
+        });
+      } catch (err) {
+        console.log("🚀 ~ file: ReviewModal.tsx:33 ~ handleSubmit ~ err:", err);
+      }
+    } else {
+      let errorMessage = "Please fill in the following:";
+      if (!rating) errorMessage += "\n- Rating";
+      if (!title) errorMessage += "\n- Title";
+      if (!body) errorMessage += "\n- Body";
+
+      Alert.alert("Incomplete Review", errorMessage, [
+        { text: "OK", onPress: () => console.log("close") },
+      ]);
     }
   };
 
@@ -71,10 +84,13 @@ const ReviewModal = (props: Iprops) => {
         <View className="w-[90%] mx-[5%] h-[60%] bg-slate-300 absolute inset-x-0 bottom-0 rounded-t-xl p-2 shadow-2xl">
           <View>
             <Pressable
-              className="justify-self-end ml-[96%]"
-              onPress={() => setModalVisible(!modalVisible)}
+              className="justify-self-end ml-[94%]"
+              onPress={() => {
+                setModalVisible(!modalVisible);
+                setRating(0);
+              }}
             >
-              <Text className="text-right">❌</Text>
+              <AntDesign name="close" size={24} color="black" />
             </Pressable>
 
             <View>
@@ -86,22 +102,24 @@ const ReviewModal = (props: Iprops) => {
                 onValueChange={setRating}
                 className="h-10 my-2"
                 step={1}
+                value={rating}
               />
               <Text>{rating}</Text>
             </View>
 
             <TextInput
               className="bg-white p-2 m-3 h-10 rounded-md"
-              placeholder="title"
+              placeholder="Enter a catchy title for your review"
               placeholderTextColor={"#0008"}
               value={title}
               onChangeText={handleTitle}
             />
             <TextInput
               className="bg-white p-2 m-3 rounded-md min-h-fit"
-              placeholder="body"
+              placeholder="Share your thoughts about the music here"
               placeholderTextColor={"#0008"}
               value={body}
+              numberOfLines={4}
               onChangeText={handleBody}
               multiline={true}
             />
@@ -109,18 +127,19 @@ const ReviewModal = (props: Iprops) => {
               onPress={handleSubmit}
               className="bg-red-200 w-40 p-6 rounded-md mx-auto"
             >
-              <Text className="text-center">submit</Text>
+              <Text className="text-center">Submit Review</Text>
             </Pressable>
           </View>
         </View>
       </Modal>
       <Pressable
-        className="bg-red-200 w-40 p-6 rounded-md mx-auto"
+        className="bg-red-200 w-40 p-6 rounded-md mx-auto mt-3"
         onPress={() => setModalVisible(true)}
       >
-        <Text className="text-center">Show Modal</Text>
+        <Text className="text-center">Write a Review</Text>
       </Pressable>
     </View>
   );
 };
+
 export default ReviewModal;
